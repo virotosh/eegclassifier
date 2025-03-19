0c3c901249945127b7ef735ca89260a570410e4a
virotosh <vuongthanhtung@live.com>
2025-03-19 21:23:05 +0200

    init

--- util/helpers.py
+++ util/helpers.py
@@ -14,16 +14,16 @@ class helpers:
             tmp_data = data[cls_idx]
             tmp_label = label[cls_idx]
 
-            tmp_aug_data = np.zeros((int(self.batch_size / 2), 1, 3, 1000))
+            tmp_aug_data = np.zeros((int(batch_size / 2), 1, 3, 1000))
 
-            for ri in range(int(self.batch_size / 2)):
+            for ri in range(int(batch_size / 2)):
                 for rj in range(8):
                     rand_idx = np.random.randint(0, tmp_data.shape[0], 8)
                     tmp_aug_data[ri, :, :, rj * 125:(rj + 1) * 125] = tmp_data[rand_idx[rj], :, :,
                                                                       rj * 125:(rj + 1) * 125]
 
             aug_data.append(tmp_aug_data)
-            aug_label.append(tmp_label[:int(self.batch_size / 2)])
+            aug_label.append(tmp_label[:int(batch_size / 2)])
         aug_data = np.concatenate(aug_data)
         aug_label = np.concatenate(aug_label)
         aug_shuffle = np.random.permutation(len(aug_data))
